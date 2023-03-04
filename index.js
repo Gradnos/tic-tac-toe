43b47ede90cc2703be401a1b4166bee21002ebcd
Gradnos <sdolaqidze@gmail.com>
2023-03-04 13:36:12 +0400

    Create automatic board fill function

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const gameBoard = (() =>{
     let array = [];
+    let board = document.querySelector(".board");
+    let arraySize = null;
+    let r = document.documentElement;
+    let boxStyle = "box";
+
     const createEmptyArray = (size) => {
         array = new Array(size);
+        board.innerHTML = "";
+        arraySize = size;
+        r.style.setProperty("--board-divisions", size);
         for(let i = 0; i < size; i++){
             array[i] = new Array(size).fill(null);
         }
+        fillBoard();
     };
     const setIcon = (xCoord, yCoord, icon) => {
         array[yCoord][xCoord] = icon;
+    };
+    const fillBoard = () => {
+        for(let i = 0; i < arraySize*arraySize; i++){
+            let newBox = document.createElement("div");
+            newBox.classList.add(boxStyle);
+            board.appendChild(newBox);
+        }
     };
 
 
@@ -17,4 +33,4 @@ const gameBoard = (() =>{
     };
 })();
 
-gameBoard.createEmptyArray(3);
+//gameBoard.createEmptyArray(3);
